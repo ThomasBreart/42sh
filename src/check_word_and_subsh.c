5d6bafcf70c4709e20519a0f8bbeb9d6da337ca4
Thomas BREART <tbreart@student.42.fr>
2016-12-03 19:40:25 +0100

    fix `() `` ``` et fix `() ()`

--- src/check_word_and_subsh.c
+++ src/check_word_and_subsh.c
@@ -6,7 +6,7 @@
 /*   By: tbreart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 05:43:23 by tbreart           #+#    #+#             */
-/*   Updated: 2016/11/17 05:44:30 by tbreart          ###   ########.fr       */
+/*   Updated: 2016/12/03 19:37:16 by tbreart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int		check_word_and_subsh(t_list *first)
 {
 	while (first != NULL)
 	{
-		if (first->type == LEX_WORD)
+		if (first->type == LEX_WORD || first->type == LEX_SUBSH)
 		{
-			if (first->next != NULL && first->next->type == LEX_SUBSH)
+			if (first->next != NULL &&
+			(first->next->type == LEX_SUBSH || first->next->type == LEX_WORD))
 			{
 				ft_putendl_fd("Invalid command.", STDERR_FILENO);
 				return (-1);
